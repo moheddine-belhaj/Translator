6fe973be5fb982eeee5aeab90bb941c796c002e3
Moheddine <60616758+moheddine-belhaj@users.noreply.github.com>
2024-07-22 15:12:02 +0000

    Service Layer Method

---
+++ account/service/user_services.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jacobsngoodwin/memrizr/account/model"
+)
+
+// UserService acts as a struct for injecting an implementation of UserRepository
+// for use in service methods
+type UserService struct {
+	UserRepository model.UserRepository
+}
+
+// Get retrieves a user based on their UUID
+func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, error) {
+	u, err := s.UserRepository.FindByID(ctx, uid)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+// NewUserService initializes a UserService with its repository layer dependencies
+func NewUserService(userRepo model.UserRepository) *UserService {
+	return &UserService{
+		UserRepository: userRepo,
+	}
+}
