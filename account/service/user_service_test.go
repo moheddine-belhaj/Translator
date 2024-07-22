3f5cac6b82805e1ed106c23a6eff911cfd484d3f
Moheddine <60616758+moheddine-belhaj@users.noreply.github.com>
2024-07-22 15:11:03 +0000

    Testing a Service Layer Method

---
+++ account/service/user_service_test.go
@@ -0,0 +1,51 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/jacobsngoodwin/memrizr/account/model"
+	"github.com/jacobsngoodwin/memrizr/account/model/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGet(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		uid, _ := uuid.NewRandom()
+
+		mockUserResp := &model.User{
+			UID:   uid,
+			Email: "bob@bob.com",
+			Name:  "Bobby Bobson",
+		}
+
+		mockUserRepository := new(mocks.MockUserRepository)
+		us := NewUserService(mockUserRepository)
+		mockUserRepository.On("FindByID", mock.Anything, uid).Return(mockUserResp, nil)
+
+		ctx := context.TODO()
+		u, err := us.Get(ctx, uid)
+
+		assert.NoError(t, err)
+		assert.Equal(t, mockUserResp, u)
+		mockUserRepository.AssertExpectations(t)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		uid, _ := uuid.NewRandom()
+
+		mockUserRepository := new(mocks.MockUserRepository)
+		us := NewUserService(mockUserRepository)
+		mockUserRepository.On("FindByID", mock.Anything, uid).Return(nil, fmt.Errorf("some error down the call chain"))
+
+		ctx := context.TODO()
+		u, err := us.Get(ctx, uid)
+
+		assert.Nil(t, u)
+		assert.Error(t, err)
+		mockUserRepository.AssertExpectations(t)
+	})
+}
