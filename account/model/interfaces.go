e57bed88758b8430e2f05f2c04ef9b5ebd63f0b7
Moheddine <60616758+moheddine-belhaj@users.noreply.github.com>
2024-07-23 12:55:41 +0000

    need to be fixed

--- account/model/interfaces.go
+++ account/model/interfaces.go
@@ -1,12 +1,14 @@
 package model
 
-import(
+import (
 	"context"
+
 	"github.com/google/uuid"
 )
 
 type UserService interface {
 	Get(ctx context.Context, uid uuid.UUID) (*User, error)
+	Signup(ctx context.Context, u *User) error
 }
 type UserRepository interface {
 	FindById(ctx context.Context, uid uuid.UUID) (*User, error)
--- account/service/user_services.go
+++ account/service/user_services.go
@@ -13,13 +13,11 @@ type UserService struct {
 	UserRepository model.UserRepository
 }
 
-// Get retrieves a user based on their UUID
-func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, error) {
-	u, err := s.UserRepository.FindByID(ctx, uid)
-	if err != nil {
-		return nil, err
-	}
-	return u, nil
+
+// USConfig will hold repositories that will eventually be injected into this
+// this service layer
+type USConfig struct {
+	UserRepository model.UserRepository
 }
 
 // NewUserService initializes a UserService with its repository layer dependencies
@@ -28,6 +26,23 @@ func NewUserService(userRepo model.UserRepository) *UserService {
 		UserRepository: userRepo,
 	}
 }
+
+// func NewUserService(c *USConfig) model.UserService {
+// 	return &UserService{
+// 		UserRepository: c.UserRepository,
+// 	}
+// }	
+
+
+// Get retrieves a user based on their UUID
+func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, error) {
+	u, err := s.UserRepository.FindByID(ctx, uid)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	return u, err
+}
+
 
 
 func (s *UserService) Signup(ctx context.Context, u *model.User) error {
