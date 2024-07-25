7e9546054325c5d081ed0cb681b0c4fec562a148
moheddine-belhaj <moheddinebelhaj@gmail.com>
2024-07-25 13:34:40 +0200

    Revert "need to be fixed"

---
+++ account/model/interfaces.go
@@ -0,0 +1,13 @@
+package model
+
+import(
+	"context"
+	"github.com/google/uuid"
+)
+
+type UserService interface {
+	Get(ctx context.Context, uid uuid.UUID) (*User, error)
+}
+type UserRepository interface {
+	FindById(ctx context.Context, uid uuid.UUID) (*User, error)
+}
---
+++ account/service/user_services.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/moheddine-belhaj/Translator/account/model"
+)
+
+// UserService acts as a struct for injecting an implementation of UserRepository
+// for use in service methods
+type UserService struct {
+	UserRepository model.UserRepository
+}
+
+// Get retrieves a user based on their UUID
+func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, error) {
+	u, err := s.UserRepository.FindByID(ctx, uid)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+// NewUserService initializes a UserService with its repository layer dependencies
+func NewUserService(userRepo model.UserRepository) *UserService {
+	return &UserService{
+		UserRepository: userRepo,
+	}
+}
+
+
+func (s *UserService) Signup(ctx context.Context, u *model.User) error {
+	panic("Method not implemeted")
+}
