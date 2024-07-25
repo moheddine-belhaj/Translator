cae8952a77ae9b45da79cc58a21e39788c5e393e
moheddine-belhaj <moheddinebelhaj@gmail.com>
2024-07-26 00:29:04 +0200

    Implement Signup in Service and Repository Layers + test

--- account/service/user_services.go
+++ account/service/user_services.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"log"
 
 	"github.com/google/uuid"
 	"github.com/moheddine-belhaj/Translator/account/model"
+	"github.com/moheddine-belhaj/Translator/account/model/apperrors"
 )
 
 
@@ -33,8 +35,24 @@ func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, erro
 	return u, err
 }
 
-// Signup reaches our to a UserRepository to verify the
-// email address is available and signs up the user if this is the case
+// Signup reaches our to a UserRepository to sign up the user.
+// UserRepository Create should handle checking for user exists conflicts
 func (s *UserService) Signup(ctx context.Context, u *model.User) error {
-	panic("Method not implemented")
+    pw, err := hashPassword(u.Password)
+
+    if err != nil {
+        log.Printf("Unable to signup user for email: %v\n", u.Email)
+        return apperrors.NewInternal()
+    }
+
+    // now I realize why I originally used Signup(ctx, email, password)
+    // then created a user. It's somewhat un-natural to mutate the user here
+    u.Password = pw
+    if err := s.UserRepository.Create(ctx, u); err != nil {
+        return err
+    }
+
+    // ...
+
+    return nil
 }
